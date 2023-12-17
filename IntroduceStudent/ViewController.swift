bfb4fcae84246786f006850fd8ebfd4e34726981
caowens <caowens555@yahoo.com>
2023-12-17 12:15:59 -0600

    Initial Commit

---
+++ IntroduceStudent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntroduceStudent
+//
+//  Created by Alex Owens on 12/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
